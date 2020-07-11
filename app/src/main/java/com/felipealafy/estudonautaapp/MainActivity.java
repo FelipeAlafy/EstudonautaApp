d2df81c3052ce0ed7d085aeb4b6a5f9d15615840
FelipeAlafy(falaf) <42254063+FelipeAlafy@users.noreply.github.com>
2020-07-11 11:26:44 -0300

    Bug Fixed
    $ erro quando clico em mídia acabo sendo redirecionado para a pagina de Equipe.
    
    $ Erro corrigido.

--- app/src/main/java/com/felipealafy/estudonautaapp/MainActivity.java
+++ app/src/main/java/com/felipealafy/estudonautaapp/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void clickMidia(View view) {
-        tela = new Intent(getApplicationContext(), ActivityEquipe.class);
+        tela = new Intent(getApplicationContext(), ActivityMidia.class);
         startActivity(tela);
     }
 }
