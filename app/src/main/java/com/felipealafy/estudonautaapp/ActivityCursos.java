5c84ffa0a38d1975c0018668a10c5eb2058a95fc
FelipeAlafy(falaf) <42254063+FelipeAlafy@users.noreply.github.com>
2020-07-12 09:59:02 -0300

    Adições
    + Adicionado onClick para todas as imagens de curso.
    + Adicionado Intent para abrir o navegador quando clicado em um curso, direto no curso.

--- app/src/main/java/com/felipealafy/estudonautaapp/ActivityCursos.java
+++ app/src/main/java/com/felipealafy/estudonautaapp/ActivityCursos.java
@@ -2,11 +2,13 @@ package com.felipealafy.estudonautaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
 public class ActivityCursos extends AppCompatActivity {
-
+    Intent nav;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +18,40 @@ public class ActivityCursos extends AppCompatActivity {
 
     public void back(View view) {
         finish();
+    }
+
+    public void clickCursoPortugol(View view) {
+        nav = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.estudonauta.com/course-cat/programacao-basica/"));
+        startActivity(nav);
+    }
+
+    public void clickCursoLingC(View view) {
+        nav = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.estudonauta.com/course-cat/linguagem-c/"));
+        startActivity(nav);
+    }
+
+    public void clickCursoKotlin(View view) {
+        nav = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.estudonauta.com/course-cat/kotlin/"));
+        startActivity(nav);
+    }
+
+    public void clickCursoCursoLingCSharp(View view) {
+        nav = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.estudonauta.com/course-cat/c/"));
+        startActivity(nav);
+    }
+
+    public void clickCursoAndroid(View view) {
+        nav = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.estudonauta.com/course-cat/android-studio/"));
+        startActivity(nav);
+    }
+
+    public void clickCursoPHP(View view) {
+        nav = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.estudonauta.com/course-cat/php-mysql/"));
+        startActivity(nav);
+    }
+
+    public void clickCursoProdVideos(View view) {
+        nav = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.estudonauta.com/course-cat/producao-de-videos/"));
+        startActivity(nav);
     }
 }
