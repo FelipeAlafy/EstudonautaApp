11f72e73ebd0f223f2b4e145ac4d7a5f328e2276
FelipeAlafy(falaf) <42254063+FelipeAlafy@users.noreply.github.com>
2020-07-12 11:24:41 -0300

    Adicionado VideoView
    + Adicionado Video na acivity midia, roda automaticamente ao entrar na mesma.

--- app/src/main/java/com/felipealafy/estudonautaapp/ActivityMidia.java
+++ app/src/main/java/com/felipealafy/estudonautaapp/ActivityMidia.java
@@ -2,15 +2,27 @@ package com.felipealafy.estudonautaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.VideoView;
 
 public class ActivityMidia extends AppCompatActivity {
 
+    VideoView video;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_midia);
+        video = findViewById(R.id.video);
+
+        //Confingurando o caminho do vídeo, e convertendo o mesmo para Uri.
+        String cam = "android.resource://com.felipealafy.estudonautaapp/" + R.raw.estudonauta;
+        Uri arq = Uri.parse(cam);
+
+        //Colocando o vídeo e iniciando o mesmo.
+        video.setVideoURI(arq);
+        video.start();
     }
 
     public void back(View view) {
